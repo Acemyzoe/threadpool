d365bf3a44ee3045eb2a904832d57f65ecb07e38
acemyzoe <acemyzoe@outlook.com>
2022-04-24 15:01:11 +0800

    test

--- execq-master/test.cpp
+++ execq-master/test.cpp
@@ -1,33 +1,31 @@
 #include <execq/execq.h>
 #include <iostream>
-// The function is called each time the thread is ready to execute next task.
-// It is called only if stream is started.
-void ProcessNextObject(const std::atomic_bool &isCanceled)
+// The function is called in parallel on the next free thread
+// with the next object from the queue.
+size_t GetStringSize(const std::atomic_bool &isCanceled, std::string &&object)
 {
     if (isCanceled)
     {
-        std::cout << "Stream has been canceled. Skipping...";
-        return;
+        std::cout << "Queue has been canceled. Skipping object...";
+        return 0;
     }
 
-    static std::atomic_int s_someObject{0};
+    std::cout << "Processing object: " << object << '\n';
 
-    const int nextObject = s_someObject++;
-
-    std::cout << "Processing object: " << nextObject << '\n';
+    return object.size();
 }
 
 int main(void)
 {
     std::shared_ptr<execq::IExecutionPool> pool = execq::CreateExecutionPool();
 
-    std::unique_ptr<execq::IExecutionStream> stream = execq::CreateExecutionStream(pool, &ProcessNextObject);
+    std::unique_ptr<execq::IExecutionQueue<size_t(std::string)>> queue = execq::CreateConcurrentExecutionQueue<size_t, std::string>(pool, &GetStringSize);
 
-    stream->start();
+    std::future<size_t> future1 = queue->push("qwe");
+    std::future<size_t> future2 = queue->push("some string");
+    std::future<size_t> future3 = queue->push("hello future");
 
-    // Only for example purposes. Usually here (if in 'main') could be RunLoop/EventLoop.
-    // Wait until some objects are processed.
-    std::cin.get();
+    const size_t totalSize = future1.get() + future2.get() + future3.get();
 
     return 0;
 }
